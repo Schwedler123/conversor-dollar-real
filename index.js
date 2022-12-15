d56a2fc026802b5e7c494fb81bb13bc6a911bf41
Tiago Vilela <100055260+Schwedler123@users.noreply.github.com>
2022-12-15 14:58:52 -0300

    att

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const $dollar = document.querySelector('[data-js="dollar"]');
 const $real = document.querySelector('[data-js="real"]');
 const $button = document.querySelector('[data-js="button"]');
 const $display = document.querySelector('[data-js="display"]');
-let real  = 5.16;
+let real  = 5.31;
 
 
  
